c691600d01af10fd2a93919981fc10f02e7cec59
Nico Castañeda <nickcar13@gmail.com>
2021-01-22 11:42:12 -0500

    terminando callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -32,11 +32,11 @@ function fetchData(url_api, callback) {
 }
 
 // empezando el llamado de datos
-fetchData(API, function (error1, data1) {
+fetchData(API, (error1, data1) => {
   if (error1) return console.error(error1);
-  fetchData(API + data1.results[0].id, function (error2, data2) {
+  fetchData(API + data1.results[0].id, (error2, data2) => {
     if (error2) return console.error(error2);
-    fetchData(data2.origin.url, function (error3, data3) {
+    fetchData(data2.origin.url, (error3, data3) => {
       if (error3) return console.error(error3);
       console.log(data1.info.count);
       console.log(data2.name);
