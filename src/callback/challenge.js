37b351a790cd5720fd751fae77105645e61671f6
Nico Castañeda <nickcar13@gmail.com>
2021-01-21 23:17:07 -0500

    realizando las peticiones

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,6 @@
+// objeto para realizar las peticiones
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 // encargada de traer la informacion y llmar el callback
 function fetchData(url_api, callback) {
@@ -28,3 +30,17 @@ function fetchData(url_api, callback) {
   //   envio de la solicitud
   xhttp.send();
 }
+
+// empezando el llamado de datos
+fetchData(API, function (error1, data1) {
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function (error2, data2) {
+    if (error2) return console.error(error2);
+    fetchData(data2.origin.url, function (error3, data3) {
+      if (error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  });
+});
